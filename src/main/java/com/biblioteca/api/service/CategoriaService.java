f9c3f24c35d29d1a00099df14961f4a65d7c49f8
Igor Mapurunga <mapurungaigor@gmail.com>
2025-07-24 17:02:49 -0300

    Adicionado notação faltando

--- src/main/java/com/biblioteca/api/service/CategoriaService.java
+++ src/main/java/com/biblioteca/api/service/CategoriaService.java
@@ -5,6 +5,7 @@ import com.biblioteca.api.dto.response.CategoriaResponseDTO;
 import com.biblioteca.api.model.Categoria;
 import com.biblioteca.api.repository.CategoriaRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,7 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Transactional
     public CategoriaResponseDTO criarCategoria(CategoriaRequestDTO dto) {
         Categoria categoria = new Categoria();
         categoria.setNome(dto.nome());
