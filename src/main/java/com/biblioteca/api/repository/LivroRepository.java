57d2e1b833bfe5687235b21ae8ba0b366fdd76ed
Igor Mapurunga <mapurungaigor@gmail.com>
2025-07-24 17:10:30 -0300

    Adicionado metodo findByAutorId para listagem de livros por autor

--- src/main/java/com/biblioteca/api/repository/LivroRepository.java
+++ src/main/java/com/biblioteca/api/repository/LivroRepository.java
@@ -3,5 +3,9 @@ package com.biblioteca.api.repository;
 import com.biblioteca.api.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    List<Livro> findByAutorId(Long autorId);
+
 }
